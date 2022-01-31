99d981fa967c32e068d52a7f0c86ad5523d68bdb
sunisha27 <72980272+sunisha27@users.noreply.github.com>
2022-01-31 20:28:30 +0530

    Create portfelio.js

---
+++ portfelio.js
@@ -0,0 +1,11 @@
+var b = document.getElementById("sidebar");
+var c = document.getElementById("content");
+
+  function toggle(){
+b.classList.toggle("active");
+c.classList.toggle("active");
+  }
+
+
+
+  //
